09014edd1913d3245a8bdece249252a6668701da
trann <trannhukhoinguyen@gmail.com>
2025-05-13 12:49:43 +0700

    clone vi -> en

---
+++ src/database/en/speechDb.tsx
@@ -0,0 +1,28 @@
+import ShakyamuniBuddha from "./speeches/ShakyamuniBuddha.tsx";
+import LaiguoMiaoshu from "./speeches/LaiguoMiaoshu.tsx";
+import NanyangHuizhong from "./speeches/NanyangHuizhong.tsx";
+import XuanshaShibei from "./speeches/XuanshaShibei.tsx";
+import LinjiYixuan from "./speeches/LinjiYixuan.tsx";
+import FoyanQingyuan from "./speeches/FoyanQingyuan.tsx";
+import YangqiFanghui from "./speeches/YangqiFanghui.tsx";
+import YunmenWenyan from "./speeches/YunmenWenyan.tsx";
+import DeshanXuanjian from "./speeches/DeshanXuanjian.tsx";
+import GuifengZongmi from "./speeches/GuifengZongmi.tsx";
+import HuangboXiyun from "./speeches/HuangboXiyun.tsx";
+
+const speechDb = [
+  ...ShakyamuniBuddha,
+
+  ...LaiguoMiaoshu,
+  ...HuangboXiyun,
+  ...XuanshaShibei,
+  ...NanyangHuizhong,
+  ...LinjiYixuan,
+  ...FoyanQingyuan,
+  ...YangqiFanghui,
+  ...YunmenWenyan,
+  ...DeshanXuanjian,
+  ...GuifengZongmi,
+
+]
+export default speechDb;
---
+++ src/utils/en/navigation.ts
@@ -0,0 +1,54 @@
+
+const navBarLinks = [
+  { name: "Trang chủ", url: "/vi" },
+  { name: "Ngữ lục", url: "/vi/speeches" },
+  { name: "Thiền sư", url: "/vi/masters" },
+  { name: "Công án", url: "/vi/speeches" },
+  { name: "Tài liệu", url: "/vi/welcome-to-docs/" },
+];
+
+const footerLinks = [
+  {
+    section: "Tham Thiền",
+    links: [
+      { name: "Tham Thiền", url: "/vi/welcome-to-docs/" },
+    ],
+  },
+  {
+    section: "Thiền sư",
+    links: [
+      { name: "Thiền sư", url: "/vi/masters" },
+    ],
+  },
+  {
+    section: "Bộ sưu tập",
+    links: [
+      { name: "Ngữ lục", url: "/vi/speeches" },
+      { name: "Tất cả", url: "/vi/collections" },
+    ],
+  },
+  {
+    section: "Thiền đường",
+    links: [
+/*      { name: "Miền Nam", url: "/vi/monasteries/vietnam/south" },
+      { name: "Miền Trung", url: "/vi/monasteries/vietnam/middle" },
+      { name: "Miền Bắc", url: "/vi/monasteries/vietnam/north" },
+      { name: "Hải ngoại", url: "/vi/monasteries/world" },*/
+      { name: "(Đang xây dựng)", url: "#" },
+    ],
+  },
+];
+
+const socialLinks = {
+  facebook: "https://www.facebook.com/Nhohoivanhin",
+  x: "#",
+  github: "https://github.com/trannhukhoinguyen/patriarchal-zen",
+  google: "#",
+  slack: "#",
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
