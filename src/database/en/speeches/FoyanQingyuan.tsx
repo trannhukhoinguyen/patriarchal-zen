1ab8fecf07a36d9e68e807f7007e5897053ffe7b
trann <trannhukhoinguyen@gmail.com>
2025-05-13 12:58:58 +0700

    fix bug speech db

---
+++ src/database/en/speeches/FoyanQingyuan.tsx
@@ -0,0 +1,108 @@
+const FoyanQingyuan = [
+{
+    id: 48154,
+    authorName: 'Phật Nhãn Thanh Viễn',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/masters/Foyan-Qingyuan',
+    speechMain:
+      'Trong thân có sanh già bệnh chết, trên niệm có sanh trụ dị diệt, cõi nước có thành trụ hoại không. Mười hai loại việc này rất có thể kỳ đặc. ' +
+      'Phàm phu chẳng biết, vì họ nổi trôi. Đức Như Lai ra đời chỉ ra Niết bàn diệu tâm, thường lạc ngã tịnh. Giống như một viên đan sa điểm sắt thành vàng. ' +
+      'Một lời tột lý chuyển phàm thành thánh. Mười hai loại này chỉ là một pháp. Hiện định như nay là người nghe pháp rõ ràng vậy. Lại nói được đến chăng? ' +
+      'Bèn nói: “Đan quế trong trăng riêng cấm lạnh, mai lạnh trong tuyết riêng phóng hương. Xuống tòa. // ' +
+      '\n' +
+      'Từ trên chư Thánh, thấy người ưa dính trần lao, chẳng mong lìa khỏi, bèn sanh lòng thương xót, bảo đó rằng: Ông theo tiếng đuổi sắc, gọi là người cuồng. Đại chúng! Hay thay lời nói ấy. Hổ thẹn thay, chư Thánh nói thế ấy. Tuy nhiên như thế, đã là đánh mở cái túi vải rồi, chẳng thể tách hợp được. Long Môn tôi hôm nay lật ngược tung ra. Có người được đó, hằng dứt mong cầu. Bèn giơ cây gậy lên, rồi dộng xuống đất một cái, nói: Há chẳng phải là tiếng ư? Ông lúc bình thường làm sao theo? Lại giơ cây gậy lên, nói: “Há chẳng phải là sắc ư? Ông bình thường làm sao đuổi? Lại hội chăng? ' +
+      'Nếu hay theo đuổi vốn không bị trói cột, chính là người liễu đạt trong chốn tòng lâm Phiền đứng lâu. // ' +
+      '\n' +
+      'Như tăng hỏi Đức Sơn: “Thế nào là việc kỳ đặc?”. Đức Sơn nói: “Tông ta không ngữ cú, không một pháp cho người”. // ' +
+      '\n' +
+      'Gươm Thái A tiếp ngang, cắt đứt muôn cơ ở trong nắm tay đang nắm. ' +
+      'Gương Bảo Giám đang ở trên đài, hiện bày các hình bóng vô tâm. ' +
+      'Có duyên liền ứng, nên hỏi đáp để tung hoành, chẳng khiến mà hành, bèn nói hình bóng mà hiếm so lường. ' +
+      'Người có ảnh hưởng, ấy là đủ sao? Cho nên, Phật giao cho Phật, Tổ truyền cho Tổ. ' +
+      'Lại không chút gì khác, há lại có khác biệt ở Đông ở Tây sao? Chẳng lập thứ lớp, riêng truyền việc ấy. ' +
+      'Nếu chẳng phải là linh căn đốn ngộ, đại dụng hiện tiền, chưa thoát khỏi biển sâu của nghiệp mê lầm, vọng trần tự cách. // ' +
+      '\n' +
+      'Sơn tăng vừa ở trong tẩm đường (nhà bệnh), trên pháp đường (nhà thuyết pháp) không có Sơn tăng, trên tẩm đường có Sơn tăng. ' +
+      'Xuống tới pháp đường, trên pháp đường có Sơn tăng, trên tẩm đường không có Sơn tăng. ' +
+      'Có thì ngoài tâm có pháp, không thì ngoài tâm, pháp chẳng tròn. ' +
+      'Chư thượng tọa, ở dưới chỗ để y bát nghe đánh bảng liền lên pháp đường, trên pháp đường được thêm thượng tọa, dưới chỗ để y bát thiếu thượng tọa. ' +
+      'Thêm thành tăng, giảm thành diệt. ' +
+      'Diệt nên rơi vào đoạn, tăng thì rơi vào thường. ' +
+      'Người hành cước làm sao lìa được có và không, lìa được thường và đoạn? Sanh tử nghi tình rất khó thấu thoát. ' +
+      'Đây là tâm thanh tịnh của Như Lai, nên nghi phải quyết trạch, chẳng nên ngồi không. ' +
+      'Ngày tháng đổi dời như tên bắn, đời phù du như thế, người sống có bao lâu? ' +
+      'Kia đây xuất gia, làm khách nghịch trong ba cõi, nhà tranh cửa trúc, ai làm chủ đó? ' +
+      'Lãnh đạm ở chung, vắng vẻ cùng ở. ' +
+      'Vì sao? Mới gì? Lo gì? Nghĩ gì? Xuống tòa. // ' +
+      '\n' +
+      'Lại thấy chăng? Nếu thấy, thấy cái gì? Nếu thấy cây phất tử chính là phàm phu. ' +
+      'Nếu nói chẳng thấy, thì đây là cây phất tử, sao nói là chẳng thấy? ' +
+      'Đạo nhân đến chỗ này phải là nghĩ lường. ' +
+      'Đâu thể coi thường lấy ở lưu truyền. ' +
+      'Thật chẳng phải việc nhỏ, phải thật ở chỗ ngộ sáng. ' +
+      'Vì thế, Thánh nhân được việc này, đều thống lĩnh ba cõi, bốn loài, hiệu lệnh Thánh phàm, phò điện chửng vật (giúp kẻ mê mờ, cứu vớt sinh linh). // ' +
+      '\n' +
+      'Đại chúng! Từ trên các bậc Thánh vào trong cửa này rồi, mỗi mỗi đều phát ngộ vào cõi này để phá các vọng trấn. ' +
+      'Nhớ lại trên hội Linh Sơn 49 năm nói chẳng hết, rốt sau trao cho ngài Ca Diếp. ' +
+      'Khoảng 9 năm ở Thiếu Lâm, sau cùng lại trao riêng cho Nhị Tổ (Huệ Khả). ' +
+      'Về sau, ánh sáng lan truyền khắp cõi Trung Hoa, đạo pháp tràn đầy khắp cả bờ cõi. ' +
+      'Lâm Tế, Đức Sơn oai hành sấm sét đều là vì Thượng Tổ chẳng xong, nên làm cho tai họa đến con cháu. // ' +
+      '\n' +
+      'Tăng hỏi Triệu Châu: “Muôn pháp về một, một quy về đâu?”. ' +
+      'Triệu Châu đáp: “Ta ở Thanh Châu may một chiếc áo vải nặng 7 cân”. ' +
+      'Đại chúng! Tột âm bặt vấn, diệu khúc chẳng phải tiếng. ' +
+      'Toàn thân chẳng dính một tấc tơ, trần thân hoàn toàn không kỵ húy. ' +
+      'Các ông quyết phải thật sự chứng nghiệm lấy, ngay đó phải cắt đứt đầu lưỡi, buông hết thân tâm, tự nhiên sông núi vui vẻ. ' +
+      'Mắt nếu chẳng buồn ngủ thì các mộng tự trừ, tâm bằng chẳng khác thì gọi là vật gì? ' +
+      'Sống vui thích! Sống vui thích! Về nhà uống trà đi! Xuống tòa. // ' +
+      '\n' +
+      'Nếu có một người phát về nguồn chơn, 10 phương hư không hết thảy đều tiêu mất hết. ' +
+      'Chư Thánh từ trước há chẳng phát về nguồn chân sao? Vậy làm sao 10 phương thế giới hư không đến nay vẫn còn? ' +
+      'Lại nói: Bọt tan không vốn không, huống lại có ba cõi! Thân huyễn đã diệt rồi nhưng hư không diệt thì không. ' +
+      'Chúng sanh trong 3 cõi từ đây cũng không còn khốn khổ nữa. 9 loài, 4 loài làm sao được không? ' +
+      'Lại nói: “Thanh tịnh vốn sẵn vậy, vì sao bỗng sanh ra sơn hà đại địa?”. ' +
+      'Đã sanh ra sơn hà đại địa thì làm sao trở lại thanh tịnh sẵn vậy? ' +
+      'Làm sao bỏ cái chấp thấy sơn hà đại địa? Đại chúng! ' +
+      'Làm sao là phải? Im lặng giây lâu, nói: “Nước từ bờ trúc theo dòng lạnh, gió ở trong hoa thoảng hương đưa. ' +
+      'Hảo đại ca, Hảo đại ca! Về nhà." // ' +
+      '\n' +
+      'Trên đường Ngũ Đài Sơn khách đi qua rất ít. Trước nhà Phá Táo (Đọa) cảm ơn không nơi. Tuyết chôn cây bách (trước) sân, băng đông cứng nơi khe suối. ' +
+      'Lò lửa tuy ở phương Nam, chẳng vào trong hũ dưa muối nhà người. Coi đây, coi đây, ngày 30 tháng Chạp chính là Mạnh Xuân còn Đông. ' +
+      'Bọn các ông, mỗi mỗi phải cố gắng hướng tới trước. Rất kỵ tự sanh lui sụt. Xuống tòa. // ' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '' +
+      '\n' +
+      '',
+    speechType: '',
+  },
+];
+
+export default FoyanQingyuan;
---
+++ src/database/en/speeches/YangqiFanghui.tsx
@@ -0,0 +1,15 @@
+const YangqiFanghui = [
+{
+    id: 3855,
+    authorName: 'Dương Kỳ Phương Hội',
+    authorImage: '/images/wide/cloudy-mountain-peak.jpg',
+    authorLink: '/masters/Yangqi-Fanghui',
+    speechMain:
+      'Thân tâm thanh tĩnh thì các cảnh thanh tĩnh. ' +
+      'Các cảnh thanh tĩnh thì tâm thanh tĩnh, còn biết lão nhân sai chỗ nào không? ' +
+      'Đánh mất tiền ở sông thì tìm ở sông // ',
+    speechType: '',
+  },
+];
+
+export default YangqiFanghui;
