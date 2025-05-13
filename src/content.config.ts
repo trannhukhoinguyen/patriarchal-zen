16e3e6b371b0835477ba2cbdc45935d1b4d4d6e8
trann <trannhukhoinguyen@gmail.com>
2025-05-13 13:11:03 +0700

    remove blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,23 +4,6 @@ import { z, defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { glob } from 'astro/loaders';
 
-const blogCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
-  schema: ({ image }) => z.object ({
-    title: z.string(),
-    description: z.string(),
-    contents: z.array(z.string()),
-    author: z.string(),
-    role: z.string().optional(),
-    authorImage: image(),
-    authorImageAlt: z.string(),
-    pubDate: z.date(),
-    cardImage: image(),
-    cardImageAlt: z.string(),
-    readTime: z.number(),
-    tags: z.array(z.string()).optional(),
-  }),
-});
 const insightsCollection = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/insights" }),
   schema: ({ image }) => z.object ({
@@ -56,7 +39,6 @@ const mastersCollection = defineCollection({
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
-  'blog': blogCollection,
   'insights': insightsCollection,
   'speeches': speechesCollection,
   'masters': mastersCollection,
